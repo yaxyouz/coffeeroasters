4c3f491160c65d9f2dbdf1e3f06aeec2243cc201
yaxyouz <sodiqjonovbot@gmail.com>
2022-02-03 15:40:56 +0500

    the project is completed

--- js/main.js
+++ js/main.js
@@ -1,15 +1,18 @@
 const burgerToggler = document.querySelector(".header__burger");
 const navigation = document.querySelector(".header__nav");
-const form = document.querySelector(".form__content");
-const buttonClose = document.querySelector(".form__button")
-const radios = document.querySelector(".form__radios")
+const buttonOpen = document.querySelectorAll(".js-form-button")
 
 burgerToggler.addEventListener("click", function () {
     burgerToggler.classList.toggle ("header__burger--opened")
     navigation.classList.toggle ("header__nav--opened")
 })
 
-form.addEventListener("click", function () {
-    buttonClose.classList.toggle ("form__button--close")
-    radios.classList.toggle ("form__radios--open")
-})
+if(buttonOpen.length > 0) {
+    buttonOpen.forEach(btn => {
+        btn.addEventListener('click', () => {
+            btn.nextElementSibling.classList.toggle('form__radios--open');
+            btn.classList.toggle('form__button--close');
+        })
+    })
+}
+
